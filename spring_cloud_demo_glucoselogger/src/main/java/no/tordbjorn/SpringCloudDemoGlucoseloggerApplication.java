34402bed31d2ce373e1d9fce51e90cc242f18e3a
Tordbjørn Wang Eriksen <tordbjorn@tordbjorn.no>
2016-12-29 18:33:17 +0100

    initial commit

---
+++ spring_cloud_demo_glucoselogger/src/main/java/no/tordbjorn/SpringCloudDemoGlucoseloggerApplication.java
@@ -0,0 +1,12 @@
+package no.tordbjorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudDemoGlucoseloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudDemoGlucoseloggerApplication.class, args);
+	}
+}
